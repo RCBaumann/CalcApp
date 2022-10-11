3079307fdba0e9d578790d8ca411f932367dffa2
Rbaumann <rbaumann@wswork.com.br>
2022-10-10 21:34:55 -0300

    calculator done

--- app/src/main/java/com/graxa/calculadorgraxa/MainActivity.java
+++ app/src/main/java/com/graxa/calculadorgraxa/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button numeroZero,numeroUm,numeroDois,numeroTres,numeroQuatro,numeroCinco,numeroSeis,
@@ -24,6 +27,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         iniciarComponentes();
         getSupportActionBar().hide();
+
+        numeroZero.setOnClickListener(this);
+        numeroUm.setOnClickListener(this);
+        numeroDois.setOnClickListener(this);
+        numeroTres.setOnClickListener(this);
+        numeroQuatro.setOnClickListener(this);
+        numeroCinco.setOnClickListener(this);
+        numeroSeis.setOnClickListener(this);
+        numeroSete.setOnClickListener(this);
+        numeroOito.setOnClickListener(this);
+        numeroNove.setOnClickListener(this);
+        ponto.setOnClickListener(this);
+        soma.setOnClickListener(this);
+        subtracao.setOnClickListener(this);
+        multiplicacao.setOnClickListener(this);
+        divisao.setOnClickListener(this);
+
+        limpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                txtExpressao.setText("");
+                txtResultado.setText("");
+            }
+        });
+
+        backspace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TextView expressao = findViewById(R.id.txt_expressao);
+                String numeros = expressao.getText().toString();
+
+                if (!numeros.isEmpty()) {
+
+                    byte var0 = 0;
+                    int var1 = numeros.length()-1;
+                    String txtExpressao = numeros.substring(var0,var1);
+                    expressao.setText(txtExpressao);
+                }
+                txtResultado.setText(" ");
+            }
+        });
+
+        igual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    Expression expressao = new ExpressionBuilder(txtExpressao.getText().toString()).build();
+                    double resultado = expressao.evaluate();
+                    long longResult = (long) resultado;
+
+                    if (resultado == (double) longResult){
+                        txtResultado.setText((CharSequence) String.valueOf(longResult));
+                    } else {
+                        txtResultado.setText((CharSequence) String.valueOf(resultado));
+                    }
+
+                }catch (Exception e) {
+
+                }
+
+
+            }
+        });
+
     }
 
     private void iniciarComponentes(){
@@ -51,20 +121,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void addExpressao(String insert_num, boolean clear_inf) {
         if (txtResultado.getText().equals("")) {
-            txtExpressao.setText("");
+            txtExpressao.setText(" ");
         }
         if (clear_inf) {
-            txtResultado.setText("");
+            txtResultado.setText(" ");
             txtExpressao.append(insert_num);
         }else {
             txtExpressao.append(txtResultado.getText());
             txtExpressao.append(insert_num);
-            txtResultado.setText("");
+            txtResultado.setText(" ");
         }
     }
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.numero_zero:
+                addExpressao("0",true);
+                break;
+            case R.id.numero_um:
+                addExpressao("1",true);
+                break;
+            case R.id.numero_dois:
+                addExpressao("2",true);
+                break;
+            case R.id.numero_tres:
+                addExpressao("3",true);
+                break;
+            case R.id.numero_quatro:
+                addExpressao("4",true);
+                break;
+            case R.id.numero_cinco:
+                addExpressao("5",true);
+                break;
+            case R.id.numero_seis:
+                addExpressao("6",true);
+                break;
+            case R.id.numero_sete:
+                addExpressao("7",true);
+                break;
+            case R.id.numero_oito:
+                addExpressao("8",true);
+                break;
+            case R.id.numero_nove:
+                addExpressao("9",true);
+                break;
+            case R.id.ponto:
+                addExpressao(".",true);
+                break;
+            case R.id.btn_soma:
+                addExpressao("+",false);
+                break;
+            case R.id.btn_subtracao:
+                addExpressao("-",false);
+                break;
+            case R.id.btn_multiplicacao:
+                addExpressao("*",false);
+                break;
+            case R.id.btn_divisao:
+                addExpressao("/",false);
+                break;
 
+        }
     }
 }
